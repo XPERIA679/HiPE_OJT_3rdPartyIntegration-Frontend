66e8bb0df07ba1526646480cf2ab0a2324fe1ba3
XPERIA679 <sorianorobin38gmail.com>
2024-06-25 17:38:31 +0800

    Initial Frontend Integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,144 @@
 import 'package:flutter/material.dart';
+import 'services/api_service.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Laravel App',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomeScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class HomeScreen extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomeScreenState extends State<HomeScreen> {
+  final ApiService apiService = ApiService();
+  late Future<List<dynamic>> places;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    places = apiService.getGeneralList();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Places with Weather'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      body: FutureBuilder<List<dynamic>>(
+        future: places,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else {
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                final place = snapshot.data![index];
+                return Card(
+                  child: ListTile(
+                    title: Text(place['name']),
+                    subtitle: Text('${place['main']['temp']} °C'),
+                    leading: Image.network(
+                      'http://openweathermap.org/img/w/${place['weather'][0]['icon']}.png',
+                      width: 50,
+                      height: 50,
+                    ),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => PlaceDetailsScreen(
+                              geoapifyId: place['geoapifyId']),
+                        ),
+                      );
+                    },
+                  ),
+                );
+              },
+            );
+          }
+        },
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class PlaceDetailsScreen extends StatelessWidget {
+  final String geoapifyId;
+
+  PlaceDetailsScreen({required this.geoapifyId});
+
+  @override
+  Widget build(BuildContext context) {
+    final ApiService apiService = ApiService();
+    late Future<Map<String, dynamic>> placeDetails =
+        apiService.getSingleFullDetails(geoapifyId);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Place Details'),
+      ),
+      body: FutureBuilder<Map<String, dynamic>>(
+        future: placeDetails,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else {
+            final place = snapshot.data!['current'];
+            final forecast = snapshot.data!['forecast']['list'];
+
+            return Column(
+              children: [
+                ListTile(
+                  title: Text(place['name']),
+                  subtitle: Text('${place['main']['temp']} °C'),
+                  leading: Image.network(
+                    'http://openweathermap.org/img/w/${place['weather'][0]['icon']}.png',
+                    width: 50,
+                    height: 50,
+                  ),
+                ),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: forecast.length,
+                    itemBuilder: (context, index) {
+                      final item = forecast[index];
+                      return ListTile(
+                        title: Text('${item['main']['temp']} °C'),
+                        subtitle: Text(item['dt_txt']),
+                        leading: Image.network(
+                          'http://openweathermap.org/img/w/${item['weather'][0]['icon']}.png',
+                          width: 50,
+                          height: 50,
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            );
+          }
+        },
+      ),
     );
   }
 }
---
+++ lib/services/api_service.dart
@@ -0,0 +1,35 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  static const String baseUrl = 'http://192.168.56.1:8000/api';
+
+  Future<List<dynamic>> getGeneralList() async {
+    final response = await http.get(Uri.parse('$baseUrl/places'));
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception('Failed to load places');
+    }
+  }
+
+  Future<Map<String, dynamic>> getSingleCurrentDetails(
+      String geoapifyId) async {
+    final response = await http.get(Uri.parse('$baseUrl/place/$geoapifyId'));
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception('Failed to load place details');
+    }
+  }
+
+  Future<Map<String, dynamic>> getSingleFullDetails(String geoapifyId) async {
+    final response =
+        await http.get(Uri.parse('$baseUrl/place/$geoapifyId/details'));
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception('Failed to load full place details');
+    }
+  }
+}
